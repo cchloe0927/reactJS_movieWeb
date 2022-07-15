a52f9902e719b446d420f78e0d219addaade566d
hyunjeong <cchloe0927@gmail.com>
2022-07-15 15:43:03 +0900

    chapter7(1)_gitPage

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,33 +1,22 @@
-import { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Detail from './routes/Detail';
+import Home from './routes/Home';
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  useEffect(() => {
-    console.log('I run only once.');
-  }, []);
-  useEffect(() => {
-    console.log('I run when "keyword" changes.');
-  }, [keyword]);
-  useEffect(() => {
-    console.log('I run when "counter" changes.');
-  }, [counter]);
-  useEffect(() => {
-    console.log('I run when keyword & counter changes.');
-  }, [keyword, counter]);
   return (
-    <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type='text'
-        placeholder='Search here...'
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/hello'>
+          <h1>Hello</h1>
+        </Route>
+        <Route path='/movie/:id'>
+          <Detail />
+        </Route>
+        <Route path='/'>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
